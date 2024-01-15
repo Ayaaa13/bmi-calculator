5224dbf0aa2e645bf00574c299b2f9ff3466b059
Ayaaa13 <aiahnava5@gmail.com>
2024-01-15 11:43:30 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+const height = document.getElementById("#height");
+const weight = document.getElementById("#weight");
+
+heightInput = e.target.value;
+weightInput = e.target.value;
+
+BMI = weight / height ** 2;
